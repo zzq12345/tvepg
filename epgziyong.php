27cda483a7b091edf58d96b60b88cdebed69577b
zzq12345 <zzqzhj@126.com>
2025-03-31 20:27:15 +0800

    Update epgziyong.php

--- epgziyong.php
+++ epgziyong.php
@@ -16,33 +16,44 @@ $xmlFiles = [
 $mergedXml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><tv></tv>');
 
 foreach ($xmlFiles as $file) {
-    // 加载每个XML文件
+    // 检查文件是否存在
+    if (!file_exists($file)) {
+        error_log("跳过不存在的文件: $file");
+        continue;
+    }
+
+    // 加载XML文件并检查错误
     $xml = simplexml_load_file($file);
+    if ($xml === false) {
+        error_log("加载失败: $file");
+        continue;
+    }
 
     // 合并<channel>元素
-    foreach ($xml->channel as $channel) {
-        $mergedChannel = $mergedXml->addChild('channel');
-        $mergedChannel->addAttribute('id', (string)$channel['id']);
-        
-        // 转义display-name中的特殊字符
-        $displayName = htmlspecialchars((string)$channel->{'display-name'}, ENT_XML1);
-        $mergedChannel->addChild('display-name', $displayName)->addAttribute('lang', (string)$channel->{'display-name'}['lang']);
+    if (isset($xml->channel)) {
+        foreach ($xml->channel as $channel) {
+            $mergedChannel = $mergedXml->addChild('channel');
+            $mergedChannel->addAttribute('id', (string)$channel['id']);
+            
+            $displayName = htmlspecialchars((string)$channel->{'display-name'}, ENT_XML1);
+            $mergedChannel->addChild('display-name', $displayName)->addAttribute('lang', (string)$channel->{'display-name'}['lang']);
+        }
     }
 
     // 合并<programme>元素
-    foreach ($xml->programme as $programme) {
-        $mergedProgramme = $mergedXml->addChild('programme');
-        $mergedProgramme->addAttribute('start', (string)$programme['start']);
-        $mergedProgramme->addAttribute('stop', (string)$programme['stop']);
-        $mergedProgramme->addAttribute('channel', (string)$programme['channel']);
-        
-        // 转义title中的特殊字符
-        $title = htmlspecialchars((string)$programme->title, ENT_XML1);
-        $mergedProgramme->addChild('title', $title)->addAttribute('lang', (string)$programme->title['lang']);
-        
-        // 转义desc中的特殊字符（关键修改点）
-        $desc = htmlspecialchars((string)$programme->desc, ENT_XML1);
-        $mergedProgramme->addChild('desc', $desc)->addAttribute('lang', (string)$programme->desc['lang']);
+    if (isset($xml->programme)) {
+        foreach ($xml->programme as $programme) {
+            $mergedProgramme = $mergedXml->addChild('programme');
+            $mergedProgramme->addAttribute('start', (string)$programme['start']);
+            $mergedProgramme->addAttribute('stop', (string)$programme['stop']);
+            $mergedProgramme->addAttribute('channel', (string)$programme['channel']);
+            
+            $title = htmlspecialchars((string)$programme->title, ENT_XML1);
+            $mergedProgramme->addChild('title', $title)->addAttribute('lang', (string)$programme->title['lang']);
+            
+            $desc = htmlspecialchars((string)$programme->desc, ENT_XML1);
+            $mergedProgramme->addChild('desc', $desc)->addAttribute('lang', (string)$programme->desc['lang']);
+        }
     }
 }
 
